ad36dc0c4d4785609faef4ab83704410319f15e7
Eric Fiorani <efiorani305@gmail.com>
2018-05-24 16:26:00 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,4 +14,6 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a
 # img").attribute("src").value
+#project.css("p.bbcard_blurb").text
+
 create_project_hash
